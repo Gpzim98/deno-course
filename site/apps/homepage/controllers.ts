3ea3f0bc8466e7fad1d3f7ebc6504e3f47b92cdd
Gregory Pacheco <gpzim98@gmail.com>
2020-05-26 22:32:46 +0100

    Denjucks as templating system

--- site/apps/homepage/controllers.ts
+++ site/apps/homepage/controllers.ts
@@ -1,9 +1,21 @@
 import { RouterContext } from 'https://deno.land/x/oak/router.ts';
+import denjucks from "https://deno.land/x/denjucks/mod.js";
 
 export class HomeController
 {
     get(context : RouterContext)
     {
-        context.response.body = 'Welcome to the home page';
+        try {
+            var base = Deno.cwd();
+            var fullPath = base + "\\site\\apps\\homepage\\templates\\";
+            denjucks.configure(fullPath);
+            var myarray = ["Mary", "John", "Pedro"]
+            var flag = true;
+            context.response.body = denjucks.render(
+                "index.html", 
+                { var : "Deno Course", people: myarray, flag: flag});
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
