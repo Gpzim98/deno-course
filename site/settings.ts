79f0828ad0040e5b6e1be099e4d31397ea310cfe
Gregory Pacheco <gpzim98@gmail.com>
2020-06-01 19:22:07 +0100

    Changing to sqlite3

--- site/settings.ts
+++ site/settings.ts
@@ -2,17 +2,17 @@ import { Flight, NewModel } from './apps/homepage/models.ts';
 
 export var config = {
     projectname: "My site",
-    // db_setup: {
-    //     engine: 'sqlite3',
-    //     database: 'sitedb3.sqlite3'
-    // },
     db_setup: {
-        database: 'denodb',
-        engine: 'postgres',
-        username: 'postgres',
-        password: 'deno',
-        host: 'localhost',
+        engine: 'sqlite3',
+        database: 'sitedb3.sqlite3'
     },
+    // db_setup: {
+    //     database: 'denodb',
+    //     engine: 'postgres',
+    //     username: 'postgres',
+    //     password: 'deno',
+    //     host: 'localhost',
+    // },
     models: [
         Flight,
         NewModel
